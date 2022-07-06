dc8392178a5bf1b73cfa05727a648f9ba4164678
ChenYn <highestopchen@gmail.com>
2022-07-06 15:35:33 +0800

    feat: add jest deps

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import { InitialOptionsTsJest } from "ts-jest";
+
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  testMatch: [`**/*.(spec|test).(ts|tsx)`],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+} as InitialOptionsTsJest;
